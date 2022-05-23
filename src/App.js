bae0de8000aafb80c6df0a3148d3efa6d650f1ee
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-23 21:32:46 +0530

    making the selection works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+import React from "react";
 import "./assets/sass/main.css";
 import Stars from "./components/Stars";
 import starIcon from "./assets/images/icon-star.svg";
+import emoji1 from "./assets/images/1star.png";
+import emoji2 from "./assets/images/2star.png";
+import emoji3 from "./assets/images/3star.png";
+import emoji4 from "./assets/images/4star.png";
+import emoji5 from "./assets/images/5star.png";
 
 function App() {
+	const starData = [
+		{
+			id: 1,
+			value: 1,
+			imgSrc: emoji1,
+		},
+		{
+			id: 2,
+			value: 2,
+			imgSrc: emoji2,
+		},
+		{
+			id: 3,
+			value: 3,
+			imgSrc: emoji3,
+		},
+		{
+			id: 4,
+			value: 4,
+			imgSrc: emoji4,
+		},
+		{
+			id: 5,
+			value: 5,
+			imgSrc: emoji5,
+		},
+	];
+
+	const [selectedStar, setSelectedStar] = React.useState(() => "");
+
+	function currentStar(event, starId) {
+		setSelectedStar(starId);
+	}
+
 	return (
 		<main className="card-container">
 			<div className="card">
@@ -16,7 +56,11 @@ function App() {
 							Please let us know how we did with your support request. All
 							feedback is appreciated to help us improve our offering!
 						</p>
-						<Stars />
+						<Stars
+							starData={starData}
+							currentStar={currentStar}
+							selectedStar={selectedStar}
+						/>
 					</div>
 					<div className="submit-button">Submit</div>
 				</div>
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,47 +1,19 @@
 import React from "react";
 import ".././assets/sass/main.css";
-import emoji1 from ".././assets/images/1star.png";
-import emoji2 from ".././assets/images/2star.png";
-import emoji3 from ".././assets/images/3star.png";
-import emoji4 from ".././assets/images/4star.png";
-import emoji5 from ".././assets/images/5star.png";
 
-const Stars = () => {
-	const starData = [
-		{
-			id: 1,
-			value: 1,
-			imgSrc: emoji1,
-		},
-		{
-			id: 2,
-			value: 2,
-			imgSrc: emoji2,
-		},
-		{
-			id: 3,
-			value: 3,
-			imgSrc: emoji3,
-		},
-		{
-			id: 4,
-			value: 4,
-			imgSrc: emoji4,
-		},
-		{
-			id: 5,
-			value: 5,
-			imgSrc: emoji5,
-		},
-	];
-
-	// const [selectedStar, setSelectedStar] = React.useState([]);
-
+const Stars = ({ starData, currentStar, selectedStar }) => {
 	return (
 		<div className="stars">
 			{starData.map((stars) => {
 				return (
-					<div className="star" key={stars.id}>
+					<div
+						className={`star ${
+							selectedStar === stars.id ? "star-selected" : ""
+						}`}
+						key={stars.id}
+						id={`star-${stars.id}`}
+						onClick={(event) => currentStar(event, stars.id)}
+					>
 						{stars.value}
 						<img src={stars.imgSrc} alt="emojies" />
 					</div>
